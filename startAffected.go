cc0fc571463e528c71ec682f4d42827e126a8715
Pedro Brandao <pcrbrandao@gmail.com>
2017-04-28 17:11:22 -0300

    Organizando em struct

--- startAffected.go
+++ startAffected.go
@@ -1,34 +1,9 @@
 package main
 
-import (
-	"context"
-	"fmt"
-	"os/exec"
-	"time"
-)
+import u "github.com/freeality/cabineTerrorGoLang/utils"
 
+//
 func main() {
-	// iniciarAffected()
-	iniciarAffected()
-}
-
-func iniciarAffectedSemEsperar() {
-
-}
-
-func iniciarAffected() {
-	t0 := time.Now()
-	tempoEspera := 5 * time.Second
-
-	ctx, cancel := context.WithTimeout(context.Background(), tempoEspera)
-	defer cancel()
-
-	cmnd := exec.CommandContext(ctx, "notepad")
-	err := cmnd.Run()
-
-	if err != nil {
-		fmt.Printf("Código de saída...%v \n", err)
-		t1 := time.Now()
-		fmt.Printf("Tempo de execução... %v\n", t1.Sub(t0))
-	}
+	i := u.Iniciar{Programa: "notepad"}
+	i.IniciarComTicker()
 }
