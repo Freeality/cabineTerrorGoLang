c23a652886c2e987f975dac8d48051d9bd9089ed
Pedro Brandao <pcrbrandao@gmail.com>
2017-04-28 18:03:07 -0300

    iniciar finalizado

--- startAffected.go
+++ startAffected.go
@@ -1,9 +1,17 @@
 package main
 
-import u "github.com/freeality/cabineTerrorGoLang/utils"
+import (
+	u "github.com/freeality/cabineTerrorGoLang/utils"
+	"time"
+)
 
-//
+// O usuário deve poder utilizar o jogo por 5 minutos.
+// após esse tempo o jogo deve ser encerrado
+// para iniciar o jogo demora-se aproximadamente 3 minutos.
+// considera-se que o usuário utilizou o jogo a partir de 2 minutos.
+// cada vez que o usuário utiliza o jogo um registro deve ser armazenado
+// em um arquivo para consulta posterior.
 func main() {
-	i := u.Iniciar{Programa: "notepad"}
+	i := u.Iniciar{Programa: "notepad", Intervalo:time.Second * 5}
 	i.IniciarComTicker()
 }
--- utils/iniciar.go
+++ utils/iniciar.go
@@ -4,17 +4,16 @@ import (
 	"time"
 	"os/exec"
 	"fmt"
-	"log"
-	"context"
 )
 
 type Iniciar struct {
 	Programa string
+	Intervalo time.Duration
 }
 
-func (i Iniciar) IniciarComTicker() {
+// inicia o programa no intervalo de tempo da instancia
+func (i *Iniciar) IniciarComTicker() {
 	intervaloDoTick := time.Second
-	intervaloTotal := time.Second * 5
 	ticker := time.NewTicker(intervaloDoTick)
 
 	cmd := exec.Command(i.Programa)
@@ -22,47 +21,11 @@ func (i Iniciar) IniciarComTicker() {
 
 	go func() {
 		for t := range ticker.C {
-			fmt.Println("Tick em ", t)
+			fmt.Println("Tick em", t)
 		}
 	}()
 
-	time.Sleep(intervaloTotal)
+	time.Sleep(i.Intervalo)
 	ticker.Stop()
 	cmd.Process.Kill()
-}
-
-func (i Iniciar) IniciarSemEsperarComTimer() {
-	tempoDeEspera := time.Second * 5
-	t0 := time.Now()
-	timer := time.NewTimer(tempoDeEspera)
-	cmd := exec.Command("notepad")
-	err := cmd.Start()
-
-	if err != nil {
-		fmt.Errorf("Erro tentando executar %v", err)
-		return
-	}
-
-	<-timer.C
-	cmd.Process.Kill()
-	err = cmd.Wait()
-	t1 := time.Now()
-	log.Printf("Programa finalizado em %v", t1.Sub(t0))
-}
-
-func (i Iniciar) IniciarEsperando() {
-	t0 := time.Now()
-	tempoEspera := 5 * time.Second
-
-	ctx, cancel := context.WithTimeout(context.Background(), tempoEspera)
-	defer cancel()
-
-	cmnd := exec.CommandContext(ctx, "notepad")
-	err := cmnd.Run()
-
-	if err != nil {
-		fmt.Printf("Código de saída...%v \n", err)
-		t1 := time.Now()
-		fmt.Printf("Tempo de execução... %v\n", t1.Sub(t0))
-	}
 }
