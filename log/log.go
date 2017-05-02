949e162efb0212c4c9c981f277b465842eaef240
Pedro Brandao <pcrbrandao@gmail.com>
2017-05-02 14:08:25 -0300

    esta pronto

--- log/log.go
+++ log/log.go
@@ -3,7 +3,9 @@ package iniciar
 import (
 	"context"
 	"fmt"
+	"os"
 	"os/exec"
+	"strings"
 	"time"
 )
 
@@ -47,6 +49,68 @@ func (i *Log) IniciarEsperando() {
 	if err != nil {
 		fmt.Printf("Código de saída...%v \n", err)
 		t1 := time.Now()
-		fmt.Printf("Tempo de execução... %v\n", t1.Sub(t0))
+		duracao := t1.Sub(t0)
+		fmt.Printf("Tempo de execução... %v\n", duracao)
+		fmt.Printf("data: %s, hora: %s\n", tempoParaDataString(t1), tempoParaHora(t1))
+
+		home := os.ExpandEnv("${HOMEPATH}")
+
+		if len(home) > 0 {
+			criaArquivo(duracao, t1, fmt.Sprintf("%s\\logs", home))
+		}
 	}
+}
+
+// listaEnv lista todas as variáveis do sistema
+func listaEnv() {
+	for _, e := range os.Environ() {
+		pair := strings.Split(e, "=")
+		fmt.Println(pair[0])
+	}
+}
+
+// check emite panic se houver erro
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+// criarArquivo cria um arquivo com nome igual a hora
+// no formato hh-mm-ss na pasta informada, $HOME\logs,
+// com o time.Time informado
+func criaArquivo(duracao time.Duration, tempo time.Time, path string) {
+	pathCompleto := fmt.Sprintf("%s\\%s\\%s.txt", path, tempoParaDataString(tempo), tempoParaHora(tempo))
+	texto := []byte(fmt.Sprintf("%v", duracao))
+
+	fmt.Printf("crindo arquivos em %s", path)
+	errPath := os.MkdirAll(fmt.Sprintf("%s\\%s", path, tempoParaDataString(tempo)), 0777)
+	check(errPath)
+
+	f, err := os.Create(pathCompleto)
+	check(err)
+
+	defer f.Close()
+	_, err = f.Write(texto)
+	f.Sync()
+}
+
+// tempoParaDataString solicita um time.Time e retorna uma string
+// com a data no formato dd-mm-aaaa
+func tempoParaDataString(tempo time.Time) string {
+	dia := tempo.Day()
+	mes := tempo.Month()
+	ano := tempo.Year()
+
+	return fmt.Sprintf("%02d-%02d-%d", dia, mes, ano)
+}
+
+// tempoParaHora solicita um time.Time e retorna uma string
+// com a hora no formato hh-mm
+func tempoParaHora(tempo time.Time) string {
+	hora := tempo.Hour()
+	minuto := tempo.Minute()
+	segundo := tempo.Second()
+
+	return fmt.Sprintf("%d-%d-%d", hora, minuto, segundo)
 }
